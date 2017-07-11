91ef6758c29428c4fe3aa699723ce9a87e3a5b3d
Ton Swieb <ton.swieb@finalist.nl>
2017-07-11 21:50:45 +0200

    Initial check-in

---
+++ NMEA0183Handlers.cpp
@@ -0,0 +1,193 @@
+/* 
+NMEA0183Handlers.cpp
+
+2015 Copyright (c) Kave Oy, www.kave.fi  All right reserved.
+
+Author: Timo Lappalainen
+
+  This library is free software; you can redistribute it and/or
+  modify it as you like.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+ 
+#include <N2kMsg.h>
+#include <NMEA2000.h>
+#include <N2kMessages.h>
+#include <NMEA0183Messages.h>
+#include "NMEA0183Handlers.h"
+
+#define PI_2 6.283185307179586476925286766559
+
+typedef struct {
+  char *Code;
+  void (*Handler)(const tNMEA0183Msg &NMEA0183Msg); 
+} tNMEA0183Handler;
+
+// Predefinition for functions to make it possible for constant definition for NMEA0183Handlers
+void HandleRMB(const tNMEA0183Msg &NMEA0183Msg);
+void HandleRMC(const tNMEA0183Msg &NMEA0183Msg);
+void HandleGGA(const tNMEA0183Msg &NMEA0183Msg);
+void HandleHDT(const tNMEA0183Msg &NMEA0183Msg);
+void HandleVTG(const tNMEA0183Msg &NMEA0183Msg);
+
+// Internal variables
+tNMEA2000 *pNMEA2000=0;
+tBoatData *pBD=0;
+Stream* NMEA0183HandlersDebugStream=0;
+
+tNMEA0183Handler NMEA0183Handlers[]={
+  {"GGA",&HandleGGA},
+  {"HDT",&HandleHDT},
+  {"VTG",&HandleVTG},
+  {"RMC",&HandleRMC},
+  {"RMB",&HandleRMB},
+  {0,0}
+};
+
+void InitNMEA0183Handlers(tNMEA2000 *_NMEA2000, tBoatData *_BoatData) {
+  pNMEA2000=_NMEA2000;
+  pBD=_BoatData;
+}
+
+void DebugNMEA0183Handlers(Stream* _stream) {
+  NMEA0183HandlersDebugStream=_stream;
+}
+
+tN2kGNSSmethod GNSMethofNMEA0183ToN2k(int Method) {
+  switch (Method) {
+    case 0: return N2kGNSSm_noGNSS;
+    case 1: return N2kGNSSm_GNSSfix;
+    case 2: return N2kGNSSm_DGNSS;
+    default: return N2kGNSSm_noGNSS;  
+  }
+}
+
+void HandleNMEA0183Msg(const tNMEA0183Msg &NMEA0183Msg) {
+  int iHandler;
+  // Find handler
+  for (iHandler=0; NMEA0183Handlers[iHandler].Code!=0 && !NMEA0183Msg.IsMessageCode(NMEA0183Handlers[iHandler].Code); iHandler++);
+  if (NMEA0183Handlers[iHandler].Code!=0) {
+    NMEA0183Handlers[iHandler].Handler(NMEA0183Msg); 
+  }
+}
+
+double toMagnetic(double True, double Variation) {
+
+  double magnetic=True-Variation;
+  while (magnetic<0) magnetic+=PI_2;
+  while (magnetic>=PI_2) magnetic-=PI_2;
+  return magnetic;    
+}
+
+// NMEA0183 message Handler functions
+
+void HandleRMC(const tNMEA0183Msg &NMEA0183Msg) {
+  if (pBD==0) return;
+  
+  if (NMEA0183ParseRMC_nc(NMEA0183Msg,pBD->GPSTime,pBD->Latitude,pBD->Longitude,pBD->COG,pBD->SOG,pBD->DaysSince1970,pBD->Variation)) {
+    if (pNMEA2000!=0) {
+      tN2kMsg N2kMsg;
+      double MCOG = toMagnetic(pBD->COG,pBD->Variation);
+      SetN2kCOGSOGRapid(N2kMsg,1,N2khr_magnetic,MCOG,pBD->SOG); 
+      pNMEA2000->SendMsg(N2kMsg);
+      SetN2kLatLonRapid(N2kMsg,pBD->Latitude,pBD->Longitude);
+      pNMEA2000->SendMsg(N2kMsg);
+    }
+  } else if (NMEA0183HandlersDebugStream!=0) { NMEA0183HandlersDebugStream->println("Failed to parse RMC"); }
+}
+
+void HandleRMB(const tNMEA0183Msg &NMEA0183Msg) {
+  if (pBD==0) return;
+    double xte;
+    char* originID;
+    char* destID;
+    double destLatitude;
+    double destLongitude;
+    double dtw;
+    double btw;
+    double vmg;
+  
+  if (NMEA0183ParseRMB_nc(NMEA0183Msg,xte, originID, destID, destLatitude, destLongitude, dtw, btw, vmg)) {
+    if (pNMEA2000!=0) {
+      tN2kMsg N2kMsg;
+      //What is PerpendicularCrossed?
+      //Need to calc ETA time en date. Let's set it on 0.0 en 0.
+      //Need to change btw to magnetic      
+      SetN2kXTE(N2kMsg,1,N2kxtem_Autonomous, false, xte);
+      pNMEA2000->SendMsg(N2kMsg);
+      double Mbtw = toMagnetic(btw,pBD->Variation);
+      SetN2kNavigationInfo(N2kMsg,1,dtw,N2khr_magnetic,false,false,N2kdct_RhumbLine,0.0,0,0,Mbtw,0,1,destLatitude,destLongitude,vmg);
+      pNMEA2000->SendMsg(N2kMsg);
+    }
+  } else if (NMEA0183HandlersDebugStream!=0) { NMEA0183HandlersDebugStream->println("Failed to parse RMC"); }
+}
+
+void HandleGGA(const tNMEA0183Msg &NMEA0183Msg) {
+  if (pBD==0) return;
+  
+  if (NMEA0183ParseGGA_nc(NMEA0183Msg,pBD->GPSTime,pBD->Latitude,pBD->Longitude,
+                   pBD->GPSQualityIndicator,pBD->SatelliteCount,pBD->HDOP,pBD->Altitude,pBD->GeoidalSeparation,
+                   pBD->DGPSAge,pBD->DGPSReferenceStationID)) {
+    if (pNMEA2000!=0) {
+      tN2kMsg N2kMsg;
+      SetN2kGNSS(N2kMsg,1,pBD->DaysSince1970,pBD->GPSTime,pBD->Latitude,pBD->Longitude,pBD->Altitude,
+                N2kGNSSt_GPS,GNSMethofNMEA0183ToN2k(pBD->GPSQualityIndicator),pBD->SatelliteCount,pBD->HDOP,0,
+                pBD->GeoidalSeparation,1,N2kGNSSt_GPS,pBD->DGPSReferenceStationID,pBD->DGPSAge
+                );
+      pNMEA2000->SendMsg(N2kMsg); 
+    }
+
+    if (NMEA0183HandlersDebugStream!=0) {
+      NMEA0183HandlersDebugStream->print("Time="); NMEA0183HandlersDebugStream->println(pBD->GPSTime);
+      NMEA0183HandlersDebugStream->print("Latitude="); NMEA0183HandlersDebugStream->println(pBD->Latitude,5);
+      NMEA0183HandlersDebugStream->print("Longitude="); NMEA0183HandlersDebugStream->println(pBD->Longitude,5);
+      NMEA0183HandlersDebugStream->print("Altitude="); NMEA0183HandlersDebugStream->println(pBD->Altitude,1);
+      NMEA0183HandlersDebugStream->print("GPSQualityIndicator="); NMEA0183HandlersDebugStream->println(pBD->GPSQualityIndicator);
+      NMEA0183HandlersDebugStream->print("SatelliteCount="); NMEA0183HandlersDebugStream->println(pBD->SatelliteCount);
+      NMEA0183HandlersDebugStream->print("HDOP="); NMEA0183HandlersDebugStream->println(pBD->HDOP);
+      NMEA0183HandlersDebugStream->print("GeoidalSeparation="); NMEA0183HandlersDebugStream->println(pBD->GeoidalSeparation);
+      NMEA0183HandlersDebugStream->print("DGPSAge="); NMEA0183HandlersDebugStream->println(pBD->DGPSAge);
+      NMEA0183HandlersDebugStream->print("DGPSReferenceStationID="); NMEA0183HandlersDebugStream->println(pBD->DGPSReferenceStationID);
+    }
+  } else if (NMEA0183HandlersDebugStream!=0) { NMEA0183HandlersDebugStream->println("Failed to parse GGA"); }
+}
+
+void HandleHDT(const tNMEA0183Msg &NMEA0183Msg) {
+  if (pBD==0) return;
+  
+  if (NMEA0183ParseHDT_nc(NMEA0183Msg,pBD->TrueHeading)) {
+    if (pNMEA2000!=0) { 
+      tN2kMsg N2kMsg;
+      // Stupid Raymarine can not use true heading
+      double MHeading = toMagnetic(pBD->TrueHeading,pBD->Variation);
+      SetN2kMagneticHeading(N2kMsg,1,MHeading,0,pBD->Variation);
+//      SetN2kTrueHeading(N2kMsg,1,pBD->TrueHeading);
+      pNMEA2000->SendMsg(N2kMsg);
+    }
+    if (NMEA0183HandlersDebugStream!=0) {
+      NMEA0183HandlersDebugStream->print("True heading="); NMEA0183HandlersDebugStream->println(pBD->TrueHeading);
+    }
+  } else if (NMEA0183HandlersDebugStream!=0) { NMEA0183HandlersDebugStream->println("Failed to parse HDT"); }
+}
+
+void HandleVTG(const tNMEA0183Msg &NMEA0183Msg) {
+ double MagneticCOG;
+  if (pBD==0) return;
+  
+  if (NMEA0183ParseVTG_nc(NMEA0183Msg,pBD->COG,MagneticCOG,pBD->SOG)) {
+      pBD->Variation=pBD->COG-MagneticCOG; // Save variation for Magnetic heading
+    if (pNMEA2000!=0) { 
+      tN2kMsg N2kMsg;
+      SetN2kCOGSOGRapid(N2kMsg,1,N2khr_true,pBD->COG,pBD->SOG);
+      pNMEA2000->SendMsg(N2kMsg);
+      //SetN2kBoatSpeed(N2kMsg,1,SOG);
+      //NMEA2000.SendMsg(N2kMsg);
+    }
+    if (NMEA0183HandlersDebugStream!=0) {
+      NMEA0183HandlersDebugStream->print("True heading="); NMEA0183HandlersDebugStream->println(pBD->TrueHeading);
+    }
+  } else if (NMEA0183HandlersDebugStream!=0) { NMEA0183HandlersDebugStream->println("Failed to parse VTG"); }
+}
---
+++ NMEA0183ToN2k.ino
@@ -0,0 +1,89 @@
+/*
+ Demo: NMEA0183 library. NMEA0183 -> NMEA2000
+   Reads messages from NMEA0183 and forwards them to the N2k bus
+   Also forwards all NMEA2000 bus messages to the PC (Serial)
+
+ This example reads NMEA0183 messages from one serial port. It is possible
+ to add more serial ports for having NMEA0183 combiner functionality.
+
+ The messages, which will be handled has been defined on NMEA0183Handlers.cpp
+ on NMEA0183Handlers variable initialization. So this does not automatically
+ handle all NMEA0183 messages. If there is no handler for some message you need,
+ you have to write handler for it and add it to the NMEA0183Handlers variable
+ initialization. If you write new handlers, please after testing send them to me,
+ so I can add them for others use.
+*/
+
+//#define N2K_SOURCE 15
+//#define USE_N2K_MCP_CAN USE_N2K_MCP_CAN
+//#define N2k_CAN_INT_PIN 10
+//#define N2k_SPI_CS_PIN 53
+#define USE_MCP_CAN_CLOCK_SET 8
+
+
+#include <Arduino.h>
+#include <Time.h>
+#include <NMEA2000_CAN.h>  // This will automatically choose right CAN library and create suitable NMEA2000 object
+#include <N2kMessages.h>
+#include <N2kMsg.h>
+#include <NMEA2000.h>
+#include <NMEA0183.h>
+#include <NMEA0183Msg.h>
+#include <NMEA0183Messages.h>
+#include "NMEA0183Handlers.h"
+#include "BoatData.h"
+
+#define NMEA0183SourceGPSCompass 3
+#define NMEA0183SourceGPS 1
+
+tBoatData BoatData;
+
+tNMEA0183Msg NMEA0183Msg;
+tNMEA0183 NMEA0183_3;
+
+// List here messages your device will transmit.
+const unsigned long TransmitMessages[] PROGMEM={129283L,129284L,126992L,129026L,129029L,0};
+
+void setup() {
+
+  // Setup NMEA2000 system
+  Serial.begin(115200);
+  NMEA2000.SetProductInformation("00000008", // Manufacturer's Model serial code
+                                 107, // Manufacturer's product code
+                                 "NMEA0183 -> N2k -> PC",  // Manufacturer's Model ID
+                                 "1.0.0.1 (2015-11-18)",  // Manufacturer's Software version code
+                                 "1.0.0.0 (2015-11-18)" // Manufacturer's Model version
+                                 );
+  // Det device information
+  NMEA2000.SetDeviceInformation(8, // Unique number. Use e.g. Serial number.
+                                130, // Device function=PC Gateway. See codes on http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20%26%20function%20codes%20v%202.00.pdf
+                                25, // Device class=Inter/Intranetwork Device. See codes on http://www.nmea.org/Assets/20120726%20nmea%202000%20class%20%26%20function%20codes%20v%202.00.pdf
+                                2046 // Just choosen free from code list on http://www.nmea.org/Assets/20121020%20nmea%202000%20registration%20list.pdf                               
+                               );
+
+
+  NMEA2000.SetForwardStream(&Serial);
+  // If you want to use simple ascii monitor like Arduino Serial Monitor, uncomment next line
+  NMEA2000.SetForwardType(tNMEA2000::fwdt_Text); // Show in clear text. Leave uncommented for default Actisense format.
+
+  // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
+  NMEA2000.SetMode(tNMEA2000::N2km_NodeOnly,25);
+  //NMEA2000.SetDebugMode(tNMEA2000::dm_Actisense); // Uncomment this, so you can test code without CAN bus chips on Arduino Mega
+  //NMEA2000.EnableForward(false); // Disable all msg forwarding to USB (=Serial)
+  // Here we tell library, which PGNs we transmit
+  NMEA2000.ExtendTransmitMessages(TransmitMessages);
+  NMEA2000.Open();
+
+  // Setup NMEA0183 ports and handlers
+  InitNMEA0183Handlers(&NMEA2000, &BoatData);
+  NMEA0183_3.SetMsgHandler(HandleNMEA0183Msg);
+
+  NMEA0183_3.Begin(&Serial3,NMEA0183SourceGPSCompass, 4800);
+}
+
+void loop() {
+
+  NMEA2000.ParseMessages();
+  NMEA0183_3.ParseMessages();
+}
+
